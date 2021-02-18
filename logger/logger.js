14b97a901170e3e77e60c6bdaf6bb814804aecbf
Uroš Aksentijević <aksaa002@gmail.com>
2021-02-18 19:25:27 +0100

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -2,7 +2,7 @@ function datetime_now()
 {
     let date = new Date();
     let day = date.getDate().toString().padStart(2, "0");
-    let month = date.getMonth().toString().padStart(2, "0");
+    let month = (date.getMonth() + 1).toString().padStart(2, "0");
     let year = date.getFullYear().toString();
     let hours = date.getHours().toString().padStart(2, "0");
     let minutes = date.getMinutes().toString().padStart(2, "0");
